95fd2faf52bf0ec6d6ef56d35e933c5c703491af
Samuel Lair <lair001@gmail.com>
2016-12-28 17:28:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+	var counter = 0;
+
+	var addBump = function() {
+		++counter;
+	};
+
+	var getBumps = function() {
+		return counter;
+	};
+
+	return {
+		addBump, getBumps
+	};
+}
+
+function createAnimal(animalType) {
+	return function(deadlyDevice) {
+		return {
+			animalType: animalType,
+			deadlyDevice: deadlyDevice
+		};
+	};
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
