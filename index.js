6caadca3921950be8d481d4bcbbba57625386322
BriOD <briodonoghue@gmail.com>
2017-02-09 15:19:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    // debugger;
+    return { animalType, deadlyDevice };
+  }
+}
+
+var sharkCreator = createAnimal('Shark'); //returns a function
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam'); //returns an object
+var sharkWithFrickinCannon = sharkCreator('Cannon'); //object
