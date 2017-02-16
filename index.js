c966d7eb9e609cb3176280a751f3081dcbbf7bf1
AliceKB <alicekb01@gmail.com@gmail.com>
2017-02-15 23:56:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+/*jshint esversion: 6 */
+
+const app = "I don't do much.";
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  };
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
