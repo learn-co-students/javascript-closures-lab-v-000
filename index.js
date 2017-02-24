9ffcb5a50b6d930b770924ee4e87afbf6770d774
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-23 21:04:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+function bumpCounter(){
+  const counter = 0 
+
+  function addBump(){
+    return counter += 1
+  }
+
+  function getBumps(){
+    return counter 
+  }
+
+  return {
+    addBump(),
+    getBumps()
+  }
+}
