918eb500b329f31e12b679c877fa269d3c3435e5
ghaash <daniel.silverman@me.com>
2017-02-26 11:18:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+/* "scripts": {
+  "test": "mocha --timeout 5000 -R mocha-multi --reporter-options nyan=-,json=.results.json"
+}, */
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
