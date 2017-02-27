2df40d1ffab7a86244ca9b44940b663bb836ceb8
smithwebtek <brad@smithwebtek.com>
2017-02-27 17:05:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
+ 
+function bumpCounter(){
+  var counter = 0;
+  function addBump(){
+    counter+=1;
+  }
+  function getBumps(){
+    return counter;
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType, size){
+  function device(deadlyDevice){
+    return { animalType: animalType, deadlyDevice: deadlyDevice};
+  }
+  return device;
+}
