c45010470c448f8cf84c0a5a1cc795650eddee44
Emmanuel Owusu <e_owusu@live.com>
2017-03-01 19:47:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  let counter = 0;
+
+
+    function getBumps(){
+      return counter;
+    }
+
+    function addBump(){
+      return counter++
+    }
+
+    return {
+      getBumps,
+      addBump
+    };
+}
+
+
+  function createAnimal(animalType){
+      return function (deadlyDevice){
+        return {
+          animalType,
+          deadlyDevice
+        };
+      }
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+var sharkWithFrickinCannon = sharkCreator("Cannon");
