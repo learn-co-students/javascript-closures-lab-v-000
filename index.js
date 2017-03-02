f4feb2baf758ef1716bdadaab059b78a9add20cb
msosborne3 <msosborne@ymail.com>
2017-03-01 19:23:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+'use strict';
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+  
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+function createAnimal(animalType) {
+
+  function createAnimal(deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
