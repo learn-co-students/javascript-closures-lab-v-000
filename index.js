e0d886d0b2d40e3e95301c615863ad33bd4b3d37
dannyenfp <daniel.valmas@gmail.com>
2017-03-06 14:16:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+  
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon'); 
