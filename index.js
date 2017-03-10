efe541bb629955e463271cfebe282a94b4495758
Nathan Robbert <admin@bigrocketgames.com>
2017-03-09 19:52:44 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
