2b1f36fc1b55933f07e35597884764d9a28504dd
Eddie Rossiter <eddie.rossiter@gmail.com>
2017-03-20 18:38:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter++
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+
+  function createAnimal(deadlyDevice) {
+    //var diabolicalCreation = {animalType: animalType, deadlyDevice: deadlyDevice}
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+  return createAnimal
+}
+
+const sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
