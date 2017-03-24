8bc90db82859a145d2e699a2bb7f6051e2250b81
Ivan Pilot <ivanpilot@gmail.com>
2017-03-24 01:43:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter++;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  function createDeadlyDevice(deadlyDevice){
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+  return createDeadlyDevice;
+}
+
+// function sharkCreator(){
+//    createAnimal('shark')
+// }
+// var sharkCreator = () => {
+//    return createAnimal('shark')
+// }
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
