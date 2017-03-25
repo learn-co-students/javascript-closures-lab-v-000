bd1c451ebc2d9bb6f89c4de4057ac98746e87442
Pedro Acosta <pedro.acosta613@gmail.com>
2017-03-25 21:43:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0
+
+  function addBump(){
+    counter++
+  }
+
+  function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  return (deadlyDevice) => {
+    return {animalType, deadlyDevice}
+  }
+  return createAnimal
+}
+
+const sharkCreator = createAnimal("Shark")
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+const sharkWithFrickinCannon = sharkCreator("Cannon")
