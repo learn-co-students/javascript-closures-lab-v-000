f896126252f73bddb9994c9c44553776bc63f22d
ivalentine <ian@ivalentine.net>
2017-03-30 14:57:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+}
+
+var sharkCreator = createAnimal("shark")
+
+var sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam")
+
+var sharkWithFrickinCannon = createAnimal("Shark")("Cannon")
