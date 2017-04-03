14a04749729d155e372905220c4523c4beaa160e
joel <joelbitar@protonmail.com>
2017-04-03 19:21:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-const app = "I don't do much."
+// // function raspyDoorGuy() {
+// //   const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+// //
+// //   function givePassword(givenPassword) { // givePassword() is an inner function, a closure
+// //     if (givenPassword === password) { // givePassword() uses variable declared in the parent function
+// //       console.log('Ye may enter.');
+// //     } else {
+// //       console.log('Begone, landlubber!');
+// //     }
+// //   }
+// //
+// //   function willBreakPrinciples(bribeAmount) { // willBreakPrinciples() is the private method
+// //     return bribeAmount >= 50;
+// //   }
+// //
+// //   function bribe(amount) { // bribe() is an inner function, a closure
+// //     if (willBreakPrinciples(amount)) { // bribe() uses private method created in the parent function
+// //       return password; // bribe() uses variable declared in the parent function
+// //     } else {
+// //       console.log("Pssht. That won't work.");
+// //     }
+// //   }
+// //
+// //   return {
+// //     givePassword,
+// //     bribe
+// //   };
+// // }
+//
+//   // const { bribe } = raspyDoorGuy();
+//   // bribe(80);
+//   // // returns 'yarr'
+//
+// function bumpCounter() {
+//   var counter = 0;
+//
+//   function addBump() {
+//     counter++;
+//   }
+//   function getBumps() {
+//     return counter;
+//   }
+//
+//   return {
+//     addBump,
+//     getBumps
+//   };
+// }
+//
+// function createAnimal(animalType) {
+//   function x(weapon) {
+//     return {
+//       animalType: animalType,
+//       deadlyDevice: weapon
+//     };
+//   }
+// }
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function animalDevice(deadlyDevice) {
+    return { animalType: animalType, deadlyDevice: deadlyDevice };
+  }
+
+  return animalDevice;
+}
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
