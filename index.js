85adadb4fe4762be3ca38f6c496c22f7bba5f89c
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-27 11:37:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+    var counter = 0
+    
+    function addBump(){
+        counter++
+    }
+    function getBumps(){
+        return counter
+    }
+    return {
+        addBump,
+        getBumps
+    };
+}
+
+function createAnimal(type){
+    function deadlyDevice(device) {
+        return { animalType: type, deadlyDevice: device};
+    }
+    return deadlyDevice;
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
