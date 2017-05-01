a3e07f3c09342a782e097d414fae9e718acb9e22
Keegan Leitz <kjleitz@gmail.com>
2017-05-01 14:59:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter += 1;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+
+  function createDevice(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    };
+  }
+
+  return createDevice;
+}
+
+const sharkCreator = createAnimal('Shark');
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+const sharkWithFrickinCannon = sharkCreator("Cannon");
