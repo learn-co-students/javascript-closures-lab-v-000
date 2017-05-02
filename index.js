0c9333fa2faf62cceaa6e60986bc08a27a77b132
Ben Greenberg <bengreenberg@gmail.com>
2017-05-02 16:54:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+// austin powers how many bumps while driving
+
+function bumpCounter() {
+  var counter = 0; // define the counter variable
+
+  function addBump() {
+    counter +=1 // adds +1 to the counter
+}
+  function getBumps() {
+    return counter // returns the current number
+  }
+  return { addBump, getBumps }; //important it is in this order so that it adds 1 first before getting the current count
+}
+
+// dr. evil exercise
+
+function createAnimal(animalType) {
+  return function(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+var sharkWithFrickinCannon = sharkCreator("Cannon");
