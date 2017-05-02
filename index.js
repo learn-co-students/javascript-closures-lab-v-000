f8397446f7a5fb921fd0c600e1f0418c59132979
WilliamMena <williammena1991@gmail.com>
2017-05-01 23:10:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+// function createAnimal(animalType) {
+//   var newAnimal = { animalType: animalType }
+
+//   function deadlyDevice(device){
+//     newAnimal.deadlyDevice = device
+//     return newAnimal
+//   }
+
+//   return deadlyDevice
+// }
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+var sharkCreator = createAnimal("Shark")
+
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+
+var sharkWithFrickinCannon = sharkCreator("Cannon")
