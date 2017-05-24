23ba09a29dc965fe7c50cb956cb42270d98387e9
Kelsey Huse <kelseyhuse30@gmail.com>
2017-05-23 20:43:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+function bumpCounter(){
+	var counter = 0;
+
+	function addBump() {
+		counter++;
+	}
+
+	function getBumps() {
+		return counter
+	}
+
+	return {
+		addBump,
+		getBumps
+	}
+}
+
+function createAnimal(animalType){
+
+	function diabolicalCreator(deadlyDevice){
+		return {
+			animalType,
+			deadlyDevice
+		}
+	}
+
+	return diabolicalCreator;
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
