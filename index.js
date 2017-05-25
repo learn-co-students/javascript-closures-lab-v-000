8fba2be4b1ea5813855bc745db5d41ae656deab5
jennafrancis <jef399@nyu.edu>
2017-05-25 12:33:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function makeDeadly(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    };
+  }
+  return makeDeadly;
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
