ec075a020d0d2dc723ef35d0e7fc34c66ddf2542
bortch89 <bortch@gmail.com>
2017-05-26 15:02:27 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter++
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function device(deadlyDevice) {
+    return{animalType, deadlyDevice};
+  }
+
+  return device;
+}
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
