8bf16002a3b298c7610f92108559a1b3b1003cd9
mxdavis <md01@me.com>
2017-06-01 22:47:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+function bumpCounter(){
+  let counter = 0
+  function addBump() {
+    counter++
+  }
+
+  function getBumps() {
+    return counter
+  }
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  return (deadlyDevice) => {
+    return { animalType, deadlyDevice }
+  }
+}
+
+const sharkCreator = createAnimal("Shark")
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+let sharkWithFrickinCannon = sharkCreator(Cannon)
