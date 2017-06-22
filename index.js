9b31154a3f5e61636129054a138348ec520433f7
wook kim <wooke2e@gmail.com>
2017-06-22 01:22:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function bumpCounter() {
+function bumpCounter(){
   var counter = 0
   function addBump(){
     counter = counter + 1
@@ -18,8 +18,8 @@ function createAnimal(animalType) {
   function inner(deadlyDevice){
     return { animalType:animalType, deadlyDevice:deadlyDevice }
   }
-  return inner()
+  return inner
 }
 var sharkCreator = createAnimal('Shark')
-var sharkWithFrickinLaserbeam
-var sharkWithFrickinCannon
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
