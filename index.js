bb5c877e26c809a1cb5033a528644d449b288485
mikka <mikkanthrope@gmail.com>
2017-06-25 01:34:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() { return counter += 1; }
+
+  function getBumps() { return counter; }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+
+  function device(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    };
+  }
+
+  return device;
+}
+
+
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinLaserbeam = createAnimal('Shark')('Laserbeam');
+
+var sharkWithFrickinCannon = createAnimal('Shark')('Cannon');
