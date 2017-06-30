9aaf0ea2867acd873d14e1421fccbc9a5abac492
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-06-30 11:20:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var bumps = 0;
+  function addBump(){
+    bumps++;
+  }
+  function getBumps(){
+    return bumps;
+  }
+  return {addBump, getBumps};
+}
+
+function createAnimal(animalType){
+  return function(deadlyDevice){
+    return {animalType, deadlyDevice};
+  };
+}
+
+var sharkCreator = createAnimal.bind(null, 'Shark');
+var sharkWithFrickinLaserbeam = sharkCreator()('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator()('Cannon');
