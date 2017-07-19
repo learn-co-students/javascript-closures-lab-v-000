e67493dc19ad6cd4ee645542a548c05e3b224d26
Holger Paas <holger.paas@gmail.com>
2017-07-19 01:59:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump(){
+    counter += 1;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType){
+  function innerFunction(deadlyDevice){
+    return {
+      animalType,
+      deadlyDevice
+    };
+  }
+  return innerFunction;
+}
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
