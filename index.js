6e1631949bbf5576a98088c271ec5ec7e8c13353
WilliamBarela <william.barela.dev@gmail.com>
2017-07-19 10:41:53 +0900

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+// bumpCounter function with closures
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump(){
+    counter += 1;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+// How to OBJECT DESTRUCTURE
+// const{addBump, getBumps} = bumpCounter()
+// Then you can just call addBump() or getBumps()
+
+
+// Dangerous Animals
+function createAnimal(animalType){
+  return function(deadlyDevice){
+    return {animalType, deadlyDevice};
+  }
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+var sharkWithFrickinCannon = sharkCreator("Cannon");
