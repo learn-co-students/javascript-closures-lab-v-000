098d3f187cd391b605a6f61690ae2f07018f8525
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-31 10:03:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+    var counter = 0;
+
+    function addBump(num) {
+        counter++;
+    }
+    function getBumps() {
+        return counter;
+    }
+
+    return {
+        addBump,
+        getBumps
+    }
+}
+
+function createAnimal(animalType) {
+    var animal = function(deadlyDevice) {
+        return {
+            animalType: animalType,
+            deadlyDevice: deadlyDevice
+        }
+    }
+    return animal;
+}
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinCannon = sharkCreator('Cannon')
