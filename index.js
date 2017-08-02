d903cb0afd6681e6b8ab7db9d0a36825ed2c186f
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-08-02 15:10:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+  function addBump() {
+    counter++;
+  };
+  function getBumps() {
+    return counter;
+  };
+
+  return {addBump, getBumps};
+}
+
+function createAnimal(animalType) {
+  function deadlify(deadlyDevice) {
+    var deadlyAnimal = {animalType: `${animalType}`, deadlyDevice: `${deadlyDevice}` }
+    return deadlyAnimal
+  }
+  return deadlify;
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
