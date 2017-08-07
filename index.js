a5a122805528043a5e0f390e84281f017d1958d6
antley chu <antley.chu@gmail.com>
2017-08-07 00:19:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter += 1;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump, 
+    getBumps
+  };
+}
+
+
+function createAnimal(animalType){
+
+  function createAnimal(deadlyDevice){
+    return { animalType, deadlyDevice };
+  }
+  return createAnimal
+}
+
+
+var sharkCreator = createAnimal('Shark'); 
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+
+var sharkWithFrickinCannon = sharkCreator('Cannon');
