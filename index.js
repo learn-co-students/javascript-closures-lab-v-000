78cc359ea2aacb81a95e522596444f840d409a19
Aditi Green <asaki444@gmail.com>
2017-08-16 15:28:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter+=1
+  }
+
+  function getBumps(){
+    return counter
+  }
+
+   return {
+    getBumps,
+    addBump
+  };
+}
+
+function createAnimal(animalType){
+   return (deadlyDevice) => {
+         var statement =  {
+            'animalType': animalType,
+            'deadlyDevice': deadlyDevice
+           
+         }
+         return statement
+    }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinCannon = sharkCreator('Cannon')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
