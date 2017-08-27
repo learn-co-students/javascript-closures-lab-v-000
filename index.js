1e082f780813ae0278b7c16b38ae8cffe6722149
Pamela Bortnick <pbortnick@gmail.com>
2017-08-27 16:41:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function bumpCounter(counter) {
+  var counter = 0
+  function addBump(){
+    return counter++
+  }
+  function getBumps() {
+    return counter
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  return function(deadlyDevice) {
+    return {
+      createAnimal,
+      deadlyDevice
+    };
+  }
+}
+
+function sharkCreator() {
+  createAnimal('Shark')
+}
+
+var sharkWithFrickinLaserbeam = {animalType: 'Shark', deadlyDevice: 'Laserbeam'}
+
+var sharkWithFrickinCannon =  {animalType: 'Shark', deadlyDevice: 'Cannon'}
