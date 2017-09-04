1c2c4a96fc176b74f4e88aebb17a9aea99d20224
curlywallst <nnoyes@comcast.net>
2017-09-04 16:52:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter++
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+
+  function deadlyAnimal(deadlyDevice){
+    return {
+      animalType,
+      deadlyDevice
+    }
+  }
+  return deadlyAnimal
+}
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
