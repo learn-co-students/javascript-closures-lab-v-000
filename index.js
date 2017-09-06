508a4fb471a79e5147c32d3d7ab9f11f02897089
Damien de Lartigue <damien@delartigue.co.uk>
2017-09-06 15:56:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0
+
+  function addBump(){
+    return counter++
+  }
+
+  function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  return function device(deadlyDevice){
+    return {
+      animalType,
+      deadlyDevice
+    }
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
