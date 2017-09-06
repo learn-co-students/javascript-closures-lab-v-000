248637538957830da8b3110a90b7b620dd5f8ab5
Will-Sauer <will@heueso.com>
2017-09-06 15:56:04 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+function bumpCounter(){
+let counter = 0;
+  
+  function addBump() {
+  counter += 1;
+  }
+  
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+};
+
+function createAnimal(animalType) {
+  debugger
+  function equip (deadlyDevice) {
+    let nexus = {animalType: animalType, deadlyDevice: deadlyDevice}
+    return nexus;
+  }
+  return equip;
+}
+
+
+let sharkCreator = createAnimal('Shark');
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+let sharkWithFrickinCannon = sharkCreator('Cannon');
