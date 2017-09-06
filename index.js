cb7e6206589e6f14472372646232acd9ca216178
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-06 15:58:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+function bumpCounter(){
+  var counter = 0
+
+  function addBump(){
+    counter += 1
+  }
+
+  function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  var animal = animalType
+  function createAnimal(deadlyDevice){
+    var newAnimal = { animalType: animal, deadlyDevice: deadlyDevice }
+    return newAnimal
+  }
+
+  return createAnimal
+}
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+
+var sharkWithFrickinCannon = sharkCreator("Cannon")
