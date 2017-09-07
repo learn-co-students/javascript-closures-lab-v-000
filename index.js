71c1c7e7cd1ab814246e0a7ac0eb368f67ab5a2f
Dustin Anderson <dandersonsoftware@outlook.com>
2017-09-07 11:54:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter += 1;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function diabolical(deadlyDevice) {
+    return {animalType, deadlyDevice,}
+  }
+  return diabolical
+}
+
+var sharkCreator =  createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
