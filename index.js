8c57a76d9b804a3c945e0596123458e085347b90
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-27 21:52:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function bumpCounter()
+{
+  let counter = 0;
+
+  function addBump()
+  {
+    return counter++;
+  }
+
+  function getBumps()
+  {
+    return counter;
+  }
+
+  return {addBump, getBumps};
+
+}
+
+function createAnimal(animalType)
+{
+  return (deadlyDevice) => {return {animalType, deadlyDevice};}
+}
+
+var sharkCreator = createAnimal.bind(null,'Shark');
+var sharkWithFrickinLaserbeam = sharkCreator()('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator()('Cannon');
