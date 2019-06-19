af650c785b588d80f7106fd3bf274e32ae74aabf
Michael Pieschel <mpieschel39@gmail.com>
2019-06-19 19:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = "I don't do much."
 function bumpCounter(){
-  let counter;
+  let counter = 0;
+  function addBump(){
+    counter++;
+  }
+  function getBumps(){
+    counter
+  }
 }
 
 function createAnimal(){
